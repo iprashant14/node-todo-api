c00521737eb883167f4ba21e476c0341a47a8b15
iprashant14 <iprashantpandey@gmail.com>
2018-05-27 20:39:29 +0530

    added test cases for server.js

--- server/server.js
+++ server/server.js
@@ -10,9 +10,21 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res) => {
-  console.log(req.body);
+  var todo = new Todo({
+    text : req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  },(e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(3000,() => {
   console.log('Started on port 3000');
 });
+
+module.exports = {
+  app
+}
