1a511ac684ef74907f7d621857faf7a6164e1758
iprashant14 <iprashantpandey@gmail.com>
2018-05-29 22:12:51 +0530

    ADDED Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {Users} = require('./models/user');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -25,6 +26,23 @@ app.get('/todos',(req,res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   },(e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id',(req,res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if(todo){
+        res.status(200).send({todo});
+    }
+    else{
+        res.status(404).send();
+    }
+  }).catch((e) => {
     res.status(400).send(e);
   });
 });
