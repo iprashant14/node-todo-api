a7b33bc2a8b13374eb854ac1363c58bc753759da
iprashant14 <iprashantpandey@gmail.com>
2018-05-30 01:36:05 +0530

    Finish before heroku push

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise ;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://iprashant14:Pp@mlab10!@ds239940.mlab.com:39940/ppdatabase'||'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
