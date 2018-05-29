efa779405c8be73f7214fe27f5ef12f822b00e29
iprashant14 <iprashantpandey@gmail.com>
2018-05-30 01:48:53 +0530

    Change in heroku setting

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise ;
-mongoose.connect('mongodb://iprashant14:Pp@mlab10!@ds239940.mlab.com:39940/ppdatabase'||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
